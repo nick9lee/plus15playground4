242a0e7ecefb0471443390331f8cc8fbee145870
nick9lee <nicholas9lee@gmail.com>
2022-03-28 22:03:48 -0600

    idk if it is working

--- plus15playground4/plus15playground4/ViewController.swift
+++ plus15playground4/plus15playground4/ViewController.swift
@@ -8,15 +8,70 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    // documentation
+    // https://stackoverflow.com/questions/39762732/ios-10-heading-arrow-for-mkuserlocation-dot
 
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var mapView: MKMapView!
+    var headingImageView: UIImageView?
+    var locationManager: LocationManager = LocationManager()
+    
+    let locationManager2 = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        
+        locationManager2.requestWhenInUseAuthorization()
+        locationManager2.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager2.distanceFilter = kCLDistanceFilterNone
+        locationManager2.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        //mapView.showsCompass = true
+        locationManager.locationManager.startUpdatingHeading()
     }
 
+    @IBAction func buttonpressed(_ sender: Any) {
+        //center on user location
+        let userLocation = locationManager2.location
 
+        if userLocation != nil {
+            let region = MKCoordinateRegion(center: userLocation!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
+                    
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        if views.last?.annotation is MKUserLocation {
+            addHeadingView(toAnnotationView: views.last!)
+        }
+    }
+    
+    func addHeadingView(toAnnotationView annotationView: MKAnnotationView) {
+        if headingImageView == nil {
+            let image = UIImage(named: "directionalArrow")
+            headingImageView = UIImageView(image: image)
+            headingImageView!.frame = CGRect(x: (annotationView.frame.size.width - image!.size.width)/2, y: (annotationView.frame.size.height - image!.size.height)/2, width: image!.size.width, height: image!.size.height)
+            annotationView.insertSubview(headingImageView!, at: 0)
+            headingImageView!.isHidden = true
+         }
+    }
+    
+    func updateHeadingRotation() {
+        if let heading = locationManager.userHeading,
+                let headingImageView = headingImageView {
+
+                headingImageView.isHidden = false
+                let rotation = CGFloat(heading/180 * Double.pi)
+                headingImageView.transform = CGAffineTransform(rotationAngle: rotation)
+            }
+        }
 }
 
